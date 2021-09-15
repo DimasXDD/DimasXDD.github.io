120bf605fd644ec23fb076e5cc3c7e54570bc644
DimasXDD <85889340+DimasXDD@users.noreply.github.com>
2021-09-15 10:35:33 +0300

    пытаюсь пофиксить отправку
    1

--- mail.php
+++ mail.php
@@ -7,25 +7,16 @@ $mail->CharSet = 'utf-8';
 $name = $_POST['user_name'];
 $text = $_POST['user_text'];
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->isSMTP();                                      
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'dimas.site@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'az89805486002'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->SMTPAuth = true;                               
+$mail->Username = 'dimas.site@mail.ru';
+$mail->Password = 'az89805486002';
+$mail->SMTPSecure = 'ssl';                            
+$mail->Port = 25;
 
-$mail->setFrom('dimas.site@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('dmithry.sidnev.98@mail.ru');     // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+$mail->setFrom('dimas.site@mail.ru'); 
+$mail->addAddress('dmithry.sidnev.98@mail.ru');     
 
 $mail->Subject = 'Сообщение с моего сайта';
 $mail->Body    = '' .$name . ' оставил(а) сообщение.'<br>'Текст:' .$text;
@@ -34,6 +25,6 @@ $mail->AltBody = '';
 if(!$mail->send()) {
     echo 'Error';
 } else {
-    header(location: index.html);
+    echo 'Ok';
 }
 ?>
