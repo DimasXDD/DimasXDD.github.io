cb6e0029b3fe654086db6023257b3fe5785d181a
DimasXDD <85889340+DimasXDD@users.noreply.github.com>
2021-09-22 21:38:12 +0300

    1.3
    форма заработала + косметические изменения + начало для страницы "спасибо" после отправки формы

--- mail.php
+++ mail.php
@@ -1,30 +1,29 @@
-<?php 
-
-require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
-$mail->CharSet = 'utf-8';
-
-$name = $_POST['user_name'];
-$text = $_POST['user_text'];
-
-$mail->isSMTP();                                      
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               
-$mail->Username = 'dimas.site@mail.ru';
-$mail->Password = 'C6znbGL3DaaeAxEX4TSD';
-$mail->SMTPSecure = 'ssl';                            
-$mail->Port = 465;
-
-$mail->setFrom('dimas.site@mail.ru'); 
-$mail->addAddress('dmithry.sidnev.98@mail.ru');     
-
-$mail->Subject = 'Сообщение с моего сайта';
-$mail->Body    = '' .$name . ' оставил(а) сообщение.'<br>'Текст:' .$text;
-$mail->AltBody = '';
-
-if(!$mail->send()) {
-    echo 'Error';
-} else {
-    echo 'Ok';
-}
-?>
+<?php
+	require 'phpmailer/PHPMailer.php';
+	require 'phpmailer/SMTP.php';
+	require 'phpmailer/Exception.php';
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\SMTP;
+	use PHPMailer\PHPMailer\Exception;
+	$mail = new PHPMailer();
+	$mail->CharSet  = 'UTF-8';
+	$mail->isSMTP();
+	$name = $_POST['user_name'];
+	$text = $_POST['user_text'];
+	$mail->Host = "smtp.gmail.com";
+	$mail->SMTPAuth = true;
+	$mail->SMTPSecure = "tls";
+	$mail->Port = "587";
+	$mail->Username = "dimas.website69@gmail.com";
+	$mail->Password = "89805486002";
+	$mail->Subject = "Сообщение с сайта!";
+	$mail->setFrom('dimas.website69@gmail.com');
+	$mail->isHTML(true);
+	$mail->Body = "<h1>$name</h1></br><p>$text</p>";
+	$mail->addAddress('dmithry.sidnev.98@mail.ru');
+	if ( $mail->send() ) {
+		echo "Сообщение отправлено!";
+	}else{
+		echo "Ошибка: "{$mail->ErrorInfo};
+	}
+	$mail->smtpClose();
