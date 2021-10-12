bdf1ec4742033f00dcb3ad6f561faccb0172af6e
DimasXDD <85889340+DimasXDD@users.noreply.github.com>
2021-10-12 20:35:14 +0300

    перенёс кнопку найтмода

--- js/nightmode.js
+++ js/nightmode.js
@@ -1,14 +1,12 @@
 let body = document.querySelector("body");
 let navbar = document.querySelector("nav");
 let button = document.querySelector("#btn-nightmode");
-let i = document.querySelector("#i-nightmode");
 let image = document.querySelector("#img-nightmode");
 
 function NightModeToggle(){
     body.classList.add("bg-dark", "text-light");
     navbar.classList.add("navbar-dark", "bg-dark");
 
-    i.innerText = " а теперь для дневной темы";
     button.classList.remove("btn-dark");
     button.classList.add("btn-light");
     image.src = "ico\\moon_black.png";
@@ -20,7 +18,6 @@ function DayModeToggle(){
     body.classList.remove("bg-dark", "text-light");
     navbar.classList.remove("navbar-dark", "bg-dark");
 
-    i.innerText = " тыкни для тёмной темы";
     button.classList.remove("btn-light");
     button.classList.add("btn-dark");
     image.src = "ico\\moon_white.jpg";
